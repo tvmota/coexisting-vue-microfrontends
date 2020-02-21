6ac6370c098470c0244473f3861d381f4da53c37
tvmota <tiago.vieira.mota@gmail.com>
2020-02-20 23:28:43 -0300

    Modificação do dev serve do para funcionamento do app3

--- app3/src/App.vue
+++ app3/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>App3</h1>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -16,7 +17,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
